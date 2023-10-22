0c120f53ab875e514314e9db249f427442bd3681
lukor5 <oleslukasz7@gmail.com>
2023-10-22 07:30:31 +0200

    change allowed hosts

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    allowedHosts: [
+      'manage-tasks-ucko.onrender.com/'
+    ],
+  },
+};
